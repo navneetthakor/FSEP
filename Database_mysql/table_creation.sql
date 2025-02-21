bae833332a30dcac1dc0474752d06ef3472030f7
navneetthakor <tnavneet8628@gmail.com>
2025-02-21 08:57:33 +0530

    User management database

---
+++ Database_mysql/table_creation.sql
@@ -0,0 +1,39 @@
+use webpulse_stack;
+
+create table wps01(
+	-- id
+	S01F01 int primary key,
+    
+    -- fname
+    S01F02 varchar(30),
+    
+    -- lname
+    S01F03 varchar(30),
+    
+    -- password
+    S01F04 text,
+    
+    -- email
+    S01F05 varchar(50),
+    
+    -- phone
+    S01F06 varchar(10)
+);
+
+create table wps02(
+	-- server id
+    S02F01 int primary key,
+    
+    -- client id
+    S02F02 int,
+    
+    -- server_url
+    S02F03 varchar(50),
+    
+    -- server_status
+    S02F04 varchar(10)
+);
+
+
+
+
