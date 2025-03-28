c23cd91a61644d0504c32ed7c27230e1adeee2bf
navneetthakor <tnavneet8628@gmail.com>
2025-03-28 15:21:56 +0530

    small bug resolved in mstemas componenet

--- WPS_Frontend/wps_ui/src/scenes/MicrosoftTeamsIntegration.jsx
+++ WPS_Frontend/wps_ui/src/scenes/MicrosoftTeamsIntegration.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import axios from 'axios';
 // import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 
 const MicrosoftTeamsIntegration = () => {
   const [isIntegrated, setIsIntegrated] = useState(false);
 
   // Authentication Service
-import axios from 'axios';
 
 const initiateTeamsAuth = async () => {
   try {
